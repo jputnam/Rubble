6e92cb9dc5ff8bd04fb0ff258301608b5e296e88
unknown <Jared@.(none)>
2011-07-13 23:47:48 -0600

    Start on the layout rule.

---
+++ rubble/parser/Layout.scala
@@ -0,0 +1,14 @@
+package rubble.parser
+
+import rubble.data.ParseFailure
+import rubble.data.SourceLocation
+import rubble.data.Tokens._
+import rubble.data.Tokens.Bracket._
+import scala.collection.mutable.ArrayBuffer
+
+
+class Layout(private var input: ArrayBuffer[Token]) {
+    
+    
+    
+}
